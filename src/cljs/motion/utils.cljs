2282087bd8232d51409f5bf652da60bfc4534561
Chris McCormick <chris@mccormick.cx>
2016-08-26 09:45:02 +0800

    Fn to compute a partial path based on fraction or the original path.

--- src/cljs/motion/utils.cljs
+++ src/cljs/motion/utils.cljs
@@ -47,6 +47,33 @@
              (interpose '("L") (partition 2 points))
              (if closed? "Z" nil)))))
 
+(defn segment-length [[x1 y1 x2 y2]]
+  (m.sqrt (+ (m.pow (- x2 x1) 2) (m.pow (- y2 y1) 2))))
+
+(defn partial-path [amount points]
+  (cond (< (count points) 4) points
+        (= amount 1) points
+        (= amount 0) (subvec points 0 2)
+        :else (let [segment-lengths (map (fn [segment] [(segment-length segment) segment]) (partition 4 2 points))
+                    total-length (reduce + (map first segment-lengths))
+                    sub-length (* amount total-length)]
+                (loop [found (subvec points 0 2) remaining segment-lengths length 0]
+                  (if (count remaining)
+                    (let [next-segment (first remaining)
+                          segment-length (first next-segment)
+                          segment (vec (last next-segment))
+                          next-length (+ length segment-length)]
+                      (if (<= next-length sub-length)
+                        (recur
+                          (concat found (subvec segment 2))
+                          (rest remaining)
+                          next-length)
+                        (let [amount-through-segment (/ (- sub-length length) (- next-length length))
+                              [x1 y1 x2 y2] segment]
+                          (concat found [(+ x1 (* (- x2 x1) amount-through-segment))
+                                         (+ y1 (* (- y2 y1) amount-through-segment))]))))
+                    found)))))
+
 (defn timeline [duration & {:keys [atoms interval] :or {atoms {:time (atom 0) :go (atom true)} interval 16}}]
   (let [c (chan)]
     (reset! (atoms :time) 0)
