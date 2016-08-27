9b0317691baf0a8d761772bab54cad0d538cb9bf
Chris McCormick <chris@mccormick.cx>
2016-08-27 19:39:59 +0800

    Ability to customise hatch patterns.

--- src/cljs/motion/fx.cljs
+++ src/cljs/motion/fx.cljs
@@ -8,7 +8,10 @@
                         <feMergeNode/><feMergeNode in='SourceGraphic'/>
                       </feMerge>"}}])
 
-(defn component-svg-pattern-hatch []
-  [:pattern {:id "hatch" :width 7 :height 7 :patternTransform "rotate(45 0 0)" :patternUnits "userSpaceOnUse"}
-   [:line {:x1 0 :y1 0 :x2 0 :y2 7 :style {:stroke "#41A4E6" :stroke-width 2}}]])
+(defn component-svg-pattern-hatch [& [id fg bg]]
+  [:pattern {:id (or id "hatch") :width 7 :height 7 :patternTransform "rotate(45 0 0)" :patternUnits "userSpaceOnUse"}
+   [:g
+    (if bg
+      [:rect {:x 0 :y 0 :width 7 :height 7 :fill bg :stroke "none"}])
+    [:line {:x1 0 :y1 0 :x2 0 :y2 7 :stroke (or fg "#41A4E6") :stroke-width 2}]]])
 
